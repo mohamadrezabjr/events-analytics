533e5953540ce28bf44b24da56407c260f80529b
mohamadreza <mohamadrezabjr@gmail.com>
2025-09-26 15:28:03 +0330

    Event model

--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    event_name = models.CharField(max_length=100)
+    user_id = models.CharField(null=True, blank=True, max_length=256)
+    client_timestamp = models.DateTimeField(blank=True, null=True)
+    server_timestamp = models.DateTimeField(auto_now_add=True)
+    session_id = models.CharField(max_length=256, null=True, blank=True)
+    metadata = models.JSONField(null=True, blank=True)
+    def __str__(self):
+        return self.event_name, self.server_timestamp

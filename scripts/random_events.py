d408108f5d174ea5a113c69945ed3665c8337c48
mohamadreza <mohamadrezabjr@gmail.com>
2025-10-04 21:01:43 +0330

    update:random_events script

--- scripts/random_events.py
+++ scripts/random_events.py
@@ -1,36 +1,65 @@
-import random
-from datetime import datetime, timedelta
-from analytics.models import Event
 
-pages = ['/home', '/about', '/products', '/cart', 'checkout']
-browsers = ['Chrome', 'Firefox', 'Safari', 'Edge']
-devices = ['desktop', 'mobile', 'tablet']
-referrers = ['google.com', 'facebook.com', 'twitter.com', None]
-products = ['Shoes', 'Bag', 'Watch', 'Shirt', 'Hat']
+import random
+import time
+import requests
+import datetime
+import uuid
 
 events = []
 def run():
-    for i in range(50):
-        k = random.random()
 
-        event_name = 'page_view' if k < 0.6 else 'purchase' if k < 0.8 else 'button'
-        event = {
-            "event_name": event_name,
-            "user_id": random.randint(1, 20),
-            "session_id": f"session_{random.randint(1, 10)}",
-            "client_timestamp": (datetime.now() - timedelta(days=random.randint(0, 10),
-                                                            hours=random.randint(0, 23),
-                                                            minutes=random.randint(0, 59))).isoformat(),
-            "metadata": {
-                "page": random.choice(pages),
-                "browser": random.choice(browsers),
-                "device": random.choice(devices),
-                "referrer": random.choice(referrers),
-                "product_id": random.randint(1, 10),
-                "product": random.choice(products),
-            }
+
+    API_URL = "http://localhost:8000/api/events/"
+
+    EVENT_NAMES = ["page_view", "click", "purchase", "signup"]
+    DEVICES = ["desktop", "mobile", "tablet"]
+    BROWSERS = ["chrome", "firefox", "safari", "edge"]
+    PAGES = ["/home", "/about", "/products", "/checkout", "/login", "/signup"]
+    REFERRERS = ["google.com", "twitter.com", "facebook.com", "direct"]
+    PRODUCTS = ["Laptop", "Phone", "Headphones", "Keyboard", "Mouse"]
+
+    def random_event():
+        event_name = random.choice(EVENT_NAMES)
+
+        metadata = {
+            "device": random.choice(DEVICES),
+            "browser": random.choice(BROWSERS),
+            "page": random.choice(PAGES),
+            "referrer": random.choice(REFERRERS),
         }
-        if event_name == 'purchase':
-            event['metadata']['price'] = round(random.uniform(10, 500), 2)
-            event['metadata']['page'] = f'products/{event["metadata"]["product_id"]}'
-        Event.objects.create(**event)
+
+        if event_name in ["purchase", "click"] and random.random() < 0.8:
+            metadata["product_id"] = str(random.randint(100, 500))
+            metadata["product"] = random.choice(PRODUCTS)
+            metadata["price"] = round(random.uniform(10, 2000), 2) if random.random() < 0.9 else None
+            metadata["page"] = f'products/{metadata["product_id"]}'
+        if event_name == 'signup':
+            metadata["page"] = "/signup"
+        return {
+            "event_name": event_name,
+            "user_id": str(random.randint(1, 50)),
+            "session_id": str(uuid.uuid4()),
+            "timestamp": (datetime.datetime.now() - datetime.timedelta(
+                minutes=random.randint(0, 10000)
+            )).isoformat(),
+            "metadata": metadata
+        }
+
+    def generate_and_send_events(n=10):
+        for _ in range(n):
+            event = random_event()
+            s_time = time.time()
+            response = requests.post(API_URL, json=event)
+            e_time = time.time()
+            if response.status_code == 201:
+                print("✅ Event created:", f"Response time : {round((e_time - s_time)*1000, 2)} ms")
+            else:
+                print("❌ Failed:", response.status_code, response.text)
+
+    start = time.time()
+    generate_and_send_events(100)
+    end = time.time()
+    print(f"Total Response time for 100 requests : {round((end-start)*1000, 2)} ms")
+
+
+
